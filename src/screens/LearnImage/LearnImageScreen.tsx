294c0045906b00e8b37790c67dd57f039beaa0e0
lht21 <huyt61933@gmail.com>
2025-10-05 19:02:01 +0700

    bottom bar

---
+++ src/screens/LearnImage/LearnImageScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LearnImageScreen = () => {
+  return (
+    <View>
+      <Text>LearnImageScreen</Text>
+    </View>
+  )
+}
+
+export default LearnImageScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/Roadmap/RoadmapScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RoadmapScreen = () => {
+  return (
+    <View>
+      <Text>RoadmapScreen</Text>
+    </View>
+  )
+}
+
+export default RoadmapScreen
+
+const styles = StyleSheet.create({})
